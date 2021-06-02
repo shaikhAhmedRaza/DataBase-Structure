7b86ee5a744de866b45c8aaef898ad4f4b261fe4
ibrahim <mohdi9003@gmail.com>
2021-06-02 16:25:59 +0530

    02-06-2021

---
+++ HMS.Domain/ClientBankDetail.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMS.Domain
+{
+    class ClientBankDetail
+    {
+    }
+}
---
+++ HMS.Domain/UserConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMS.Domain
+{
+    class UserConfig
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
