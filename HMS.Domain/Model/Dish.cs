8c7c75122887067a145cbf2cdfe6d3edde2265fa
ibrahim <mohdi9003@gmail.com>
2021-06-03 17:34:01 +0530

    add auto mapper

---
+++ HMS.Domain/Model/Dish.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMS.Domain.Model
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public string Name { get; set; }
+        public int Description { get; set; }
+        public int HalfPrice { get; set; }
+        public int FullPrice { get; set; }
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+        public int MainCategoryId { get; set; }
+        public bool IsVeg { get; set; }
+        public bool IsHalf { get; set; }
+        public bool IsFull { get; set; }
+
+    }
+}
---
+++ HMS.Webapi/AutoMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace HMS.Webapi
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<HMS.Domain.Model.Dish, HMS.Domain.Dish>();
+        }
+    }
+}
